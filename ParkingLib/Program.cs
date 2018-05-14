e2d349c54c31bd0df698ab713931a429a26c4684
Shvidkoy Mychailo <origami690@gmail.com>
2018-05-14 23:33:30 +0300

    Добавьте файлы проекта.

---
+++ ParkingLib/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ParkingLib
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
